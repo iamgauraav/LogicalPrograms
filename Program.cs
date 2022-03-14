971a22c6ac5943d392fb3528b78a78ec33506ddf
iamgauraav <iamgauraav@gmail.com>
2022-03-14 18:15:35 +0530

    Added UC6 : To print elapsed time of a stopwatch

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
 
 namespace LogicalPrograms
 {/// <summary>
-/// UC5- Coupon Number
+/// UC6-StopWatch
 /// </summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            random cou = new random();
-            cou.Coupan();
+            //Creating new stopwatch object
+            Stopwatch stopwatch = new Stopwatch();
+
+            //Setting start timming of stopwatch by calling method start();
+            stopwatch.Start();
+
+            //For-loop
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+            //Setting stop timming of stopwatch by calling method stop();
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed:{0}",stopwatch.Elapsed);
+            Console.ReadLine();
         }
     }
 }
