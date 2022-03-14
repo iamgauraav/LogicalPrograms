b93dfdf3a82c1ce52b79e9120a0059683873d0b9
iamgauraav <iamgauraav@gmail.com>
2022-03-14 12:16:22 +0530

    Added Welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Program master branch");
+            Console.ReadLine();
+        }
+    }
+}
