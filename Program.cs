caaede731018e565532107574c2d775679fe1c64
iamgauraav <iamgauraav@gmail.com>
2022-03-14 13:09:08 +0530

    Added UC1 : Fibonacci Series

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
 namespace LogicalPrograms
-{
+{/// <summary>
+/// UC1- Fibonacci Series
+/// </summary>
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Program master branch");
+            //local variable
+            int a = 0;
+            int b = 1;
+            int c;
+
+            //taking input from user
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //For loop
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+                //swapping a and b
+                a = b;
+                b = c;
+            }
             Console.ReadLine();
         }
     }
