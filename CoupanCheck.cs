f46e24c179aa8018622d30e405679c8db31ec0d9
iamgauraav <iamgauraav@gmail.com>
2022-03-14 16:47:01 +0530

    Added UC5 : To count no. of random number and coupon number

---
+++ CoupanCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class random
+    {
+        public void Coupan()
+        {
+
+            //taking input from user
+            Console.WriteLine("Enter Coupon number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //Using random class
+            Random random = new Random();
+            int ran = random.Next(num);
+
+            //if - else condition
+            if (ran == num)
+            {
+                Console.WriteLine("coupan number {0} random number {1] " , num ,ran);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("coupan number {0} random number {1} ", num, ran);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,30 +2,15 @@
 
 namespace LogicalPrograms
 {/// <summary>
-/// UC4- Reverse a Number
+/// UC5- Coupon Number
 /// </summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            //local variable
-            int rev=0;
-            int rem = 0;
-
-            //taking input from user
-            Console.WriteLine("Enter number");
-            int num = Convert.ToInt32(Console.ReadLine());
-
-         
-            while (num > 0)
-            {
-                num = num / 10;
-                rem= num % 10;
-                rev = (rev * 10) + num;
-            }
-            Console.WriteLine("The reverse of the given number is:" + rev);
-            Console.ReadLine();
+            random cou = new random();
+            cou.Coupan();
         }
     }
 }
